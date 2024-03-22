df8931f1b8441d73dcb5c2f26103334c50278cb3
Jose-argentino <159449861+Jose-argentino@users.noreply.github.com>
2024-03-22 20:42:35 -0300

    JS Externo

---
+++ js/script-form.js
@@ -0,0 +1,9 @@
+function enviarContato() {
+
+    let name = document.getElementById('nome')
+    // criado uma variavel nome que vai receber o conteudo do id='nome'
+    // get= Pega, Element= Elemento, by=por, Id=nome, ('')=nome da id do HTML
+    alert(nome.value)
+
+
+   }
